ae75cbde0aa305d7fc4bb68d9e185899392b0014
姚泽源 <yaozeyuan93@gmail.com>
2026-06-30 23:22:16 +0800

    feat: 启用信息采集

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+import { Analytics } from "@vercel/analytics/next";
 </script>
 
 <template>
+  <Analytics></Analytics>
+  <SpeedInsights />
   <HelloWorld />
 </template>
